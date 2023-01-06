55ac432c323c1fa0f3cd46806204b4bfdbe89344
Octavian Oncescu <octavian.oncescu@gmail.com>
2023-01-06 21:53:45 +0100

    Update copyright disclaimer

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -1,5 +1,5 @@
 // Copyright (c) 2022 Octavian Oncescu
-// Copyright (c) 2022 The Purplecoin Core developers
+// Copyright (c) 2022-2023 The Purplecoin Core developers
 // Licensed under the Apache License, Version 2.0 see LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0 or the MIT license, see
 // LICENSE-MIT or http://opensource.org/licenses/MITv
