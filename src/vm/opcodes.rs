170250aeb6c22aa9dbd9b9a330fb4f1028909251
Octavian Oncescu <octavian.oncescu@gmail.com>
2023-05-01 00:30:35 +0200

    Actually add the Suspend OP

--- src/vm/opcodes.rs
+++ src/vm/opcodes.rs
@@ -11,6 +11,7 @@ use num_derive::{FromPrimitive, ToPrimitive};
 #[repr(u8)]
 pub enum OP {
     Func = 0x00,
+    Suspend = 0x05,
     ChainId = 0x07,
     ChainHeight = 0x08,
     ChainTimestamp = 0x09,
