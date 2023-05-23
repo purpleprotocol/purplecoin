c643314e573b114f6ac4187ad50e1074bbdd0900
Octavian Oncescu <octavian.oncescu@gmail.com>
2023-05-23 16:28:33 +0200

    Add ghostrider sources

--- hash_sys/build.rs
+++ hash_sys/build.rs
@@ -1,5 +1,5 @@
 // Copyright (c) 2022 Octavian Oncescu
-// Copyright (c) 2022 The Purplecoin Core developers
+// Copyright (c) 2022-2023 The Purplecoin Core developers
 // Licensed under the Apache License, Version 2.0 see LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0 or the MIT license, see
 // LICENSE-MIT or http://opensource.org/licenses/MIT
@@ -7,15 +7,34 @@
 fn main() {
     let mut build = cc::Build::new();
 
-    build.include("c_src/sph_types");
-    build.include("c_src/sph_fugue");
+    build.include("c_src/crypto/sph_types");
+    build.include("c_src/crypto/sph_fugue");
+    build.include("c_src/cryptonote/oaes_lib");
+    build.include("c_src/cryptonote/slow-hash");
+    build.include("c_src/cryptonote/c_keccak");
+    build.include("c_src/cryptonote/c_groestl");
+    build.include("c_src/cryptonote/c_blake256");
+    build.include("c_src/cryptonote/c_jh");
+    build.include("c_src/cryptonote/c_skein");
+    build.include("c_src/cryptonote/int-util");
+    build.include("c_src/cryptonote/warnings");
+    build.include("c_src/cryptonote/hash-ops");
+    build.include("c_src/cryptonote/variant2_int_sqrt");
     build.include("c_src/fugue");
-    build.file("c_src/sph_fugue.c");
+    build.include("c_src/hash_selection");
+    build.include("c_src/hash");
+    build.file("c_src/crypto/sph_fugue.c");
+    build.file("c_src/cryptonote/c_keccak.c");
+    build.file("c_src/cryptonote/c_groestl.c");
+    build.file("c_src/cryptonote/c_blake256.c");
+    build.file("c_src/cryptonote/c_jh.c");
+    build.file("c_src/cryptonote/c_skein.c");
+    build.file("c_src/cryptonote/hash-ops.c");
+    build.file("c_src/cryptonote/oaes_lib.c");
+    build.file("c_src/cryptonote/slow-hash.c");
     build.file("c_src/fugue.c");
+    build.file("c_src/hash.cpp");
 
-    if build.get_compiler().is_like_msvc() {
-        build.cpp(true);
-    }
-
-    build.compile("sph");
+    build.cpp(true);
+    build.compile("hash_sys");
 }
--- hash_sys/c_src/fugue.c
+++ hash_sys/c_src/fugue.c
@@ -1,9 +1,15 @@
+// Copyright (c) 2022 Octavian Oncescu
+// Copyright (c) 2022-2023 The Purplecoin Core developers
+// Licensed under the Apache License, Version 2.0 see LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0 or the MIT license, see
+// LICENSE-MIT or http://opensource.org/licenses/MIT
+
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
 #include "fugue.h"
-#include "sph_fugue.h"
+#include "crypto/sph_fugue.h"
 
 inline void fugue_hash(const char* input, char* output, uint32_t len)
 {   
