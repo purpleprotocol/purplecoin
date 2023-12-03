f7a3414c9f8e95df8680c701c51e20da57595eb0
Octavian Oncescu <octavian.oncescu@gmail.com>
2023-12-04 01:48:01 +0200

    Verify ed25519 batches

--- src/vm/sig_verification.rs
+++ src/vm/sig_verification.rs
@@ -47,21 +47,33 @@ pub fn verify_single_bip340(
 }
 
 pub fn verify_batch(ver_stack: &VerificationStack) -> Result<(), SigVerificationErr> {
-    unimplemented!();
+    ver_stack.ed25519.verify_batch()?;
+    Ok(())
 }
 
 #[derive(Default)]
 pub struct VerificationStack {
-    schnor: SchnorVerStack,
-    ed25519: Ed25519VerStack,
-    ecdsa: EcdsaVerStack,
-    bip340: BIP340VerStack,
+    pub(crate) schnor: SchnorVerStack,
+    pub(crate) ed25519: Ed25519VerStack,
+    pub(crate) ecdsa: EcdsaVerStack,
+    pub(crate) bip340: BIP340VerStack,
 }
 
 impl VerificationStack {
     #[must_use]
     pub fn new() -> Self {
         Default::default()
+    }
+
+    pub fn push_ed25519(
+        &mut self,
+        message: SigVerificationMessage,
+        signature: Ed25519Signature,
+        public_key: Ed25519VerifyingKey,
+    ) {
+        self.ed25519.transcripts.push(message);
+        self.ed25519.signatures.push(signature);
+        self.ed25519.public_keys.push(public_key);
     }
 }
 
@@ -79,6 +91,18 @@ struct Ed25519VerStack {
     public_keys: Vec<Ed25519VerifyingKey>,
 }
 
+impl Ed25519VerStack {
+    pub fn verify_batch(&self) -> Result<(), SigVerificationErr> {
+        let messages: Vec<_> = self
+            .transcripts
+            .iter()
+            .map(std::vec::Vec::as_slice)
+            .collect();
+        verify_batch_ed25519(&messages, &self.signatures, &self.public_keys)
+            .map_err(|_| SigVerificationErr::InvalidSignature)
+    }
+}
+
 #[derive(Default)]
 struct EcdsaVerStack {
     transcripts: Vec<SigVerificationMessage>,
@@ -92,12 +116,64 @@ struct BIP340VerStack {
     public_keys: Vec<SigVerificationPubKey>,
 }
 
-pub struct SigVerificationPubKey([u8; 32]);
-pub struct SigVerificationSignature([u8; 64]);
+pub type SigVerificationPubKey = [u8; 32];
+pub type SigVerificationSignature = [u8; 64];
 pub type SigVerificationMessage = Vec<u8>;
 pub type EcdsaRecoveryId = u8;
 
 #[derive(Clone, Debug)]
 pub enum SigVerificationErr {
     InvalidSignature,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ed25519_dalek::{Signer, SigningKey as Ed25519SigningKey};
+    use rand::rngs::OsRng;
+
+    #[test]
+    fn verify_ed25519() {
+        let mut ver_stack = VerificationStack::new();
+        let batch = (0..200_u8)
+            .map(|i| {
+                let mut csprng = OsRng;
+                let message = vec![i];
+                let signing_key = Ed25519SigningKey::generate(&mut csprng);
+                let pkey = signing_key.verifying_key();
+                let signature = signing_key.sign(&message);
+                (message, pkey, signature)
+            })
+            .collect::<Vec<_>>();
+
+        for (message, pkey, signature) in batch {
+            ver_stack.push_ed25519(message.clone(), signature, pkey);
+        }
+
+        assert!(verify_batch(&ver_stack).is_ok());
+    }
+
+    #[test]
+    fn verify_ed25519_fail_case() {
+        let mut ver_stack = VerificationStack::new();
+        let batch = (0..200_u8)
+            .map(|i| {
+                let mut csprng = OsRng;
+                let mut message = vec![i];
+                let signing_key = Ed25519SigningKey::generate(&mut csprng);
+                let pkey = signing_key.verifying_key();
+                let signature = signing_key.sign(&message);
+                if i == 0 {
+                    message = vec![0xff];
+                }
+                (message, pkey, signature)
+            })
+            .collect::<Vec<_>>();
+
+        for (message, pkey, signature) in batch {
+            ver_stack.push_ed25519(message.clone(), signature, pkey);
+        }
+
+        assert!(verify_batch(&ver_stack).is_err());
+    }
 }
